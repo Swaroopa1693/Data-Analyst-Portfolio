18a140f7e32867f406701405542c7f71405c28bb
Swaroopa Yadav <55625011+Swaroopa1693@users.noreply.github.com>
2024-02-12 11:12:52 -0600

    Update 2. EDA_US_Household_income.sql

--- Projects-SQL/P2 US Household Income/2. EDA_US_Household_income.sql
+++ Projects-SQL/P2 US Household Income/2. EDA_US_Household_income.sql
@@ -1,52 +1,86 @@
-#EDA on US household income
+# US HOUSEHOLD INCOME PROJECT: EXPLORATORY DATA ANALYSIS
 
-select * 
-from us_household_income;
+	# Table of Content: 
+		# Area of Land & Area of Water Analysis in US
+        # Average & Median Household Income Analysis in US
+        
+	USE us_house_income;
+	SELECT * FROM us_household_income; 
+	SELECT * FROM us_household_income_statistics;
+    
+    
+-- AREA OF LAND & AREA OF WATER ANALYSIS IN US --
 
-select state_name, ALand, AWater
-from us_household_income;
+	-- Part 1: Identifying Top 10 Largest States by Land -- 
+	SELECT 
+		State_Name, 
+		SUM(ALand) AS Area_of_Land
+	FROM us_household_income
+	GROUP BY State_Name
+	ORDER BY 2 DESC
+	LIMIT 10
+	;
 
-select state_name, sum(ALand), sum(AWater)
-from us_household_income
-group by state_name
-order by 3 desc;
+	-- Part 2: Identifying Top 10 Largest States by Water -- 
+	SELECT 
+		State_Name, 
+		SUM(AWater) AS Area_of_Water
+	FROM us_household_income
+	GROUP BY State_Name
+	ORDER BY 2 DESC
+	LIMIT 10
+	;
+    
+-- AVERAGE & MEDIAN HOUSEHOLD INCOME ANALYSIS IN US --
 
+	-- Part 1: Identifying the National Average & Median Household Income
+	SELECT 
+		ROUND(AVG(us.Mean)) AS Average_Income,
+		ROUND(AVG(us.Median)) AS Median_Income
+	FROM us_household_income u
+	JOIN us_household_income_statistics us
+		ON u.id = us.id
+	WHERE Mean <> 0 # Removing Outliers
+	;
 
-select state_name, ALand, AWater
-from us_household_income
-where state_name like '%Alaska%';
-
-select state_name, count(*)
-from us_household_income
-group by state_name;
-
-select *
-from us_household_income u
-right join  us_household_income_statistics us
-	on u.id = us.id
-where u.id IS NULL;
-
-#average household income state wise
-select u.state_name, round(avg(mean),2) as avg_mean, round(avg(median),2) as avg_median
-from us_household_income u
-right join  us_household_income_statistics us
-	on u.id = us.id
-where mean <> 0 
-group by u.state_name
-order by 2 desc
-limit 10 ;
-
-select type, count(type), round(avg(mean),2) as avg_mean, round(avg(median),2) as avg_median
-from us_household_income u
-inner join  us_household_income_statistics us
-	on u.id = us.id
-where mean <> 0 
-group by type
-order by avg_mean desc ;
-
-select u.state_name, city, round(avg(mean),1) as avg_mean, round(avg(median),1) as avg_median
-from us_household_income u
-right join  us_household_income_statistics us
-	on u.id = us.id
-group by u.state_name, city
-order by avg_mean desc;
+	-- Part 2: Identifying the Average and Median Income Household Income by State
+	SELECT 
+		u.State_Name,
+		ROUND(AVG(us.Mean)) AS Average_Income,
+		ROUND(AVG(us.Median)) AS Median_Income
+	FROM us_household_income u
+	JOIN us_household_income_statistics us
+		ON u.id = us.id
+	WHERE Mean <> 0 # Removing Outliers
+	GROUP BY u.State_Name
+	ORDER BY 2 DESC
+	;
+    
+	-- Part 3: Identifying the Average and Median Income Household Income by City
+	SELECT 
+        u.City,
+		u.State_Name,
+		ROUND(AVG(us.Mean)) AS Average_Income,
+		ROUND(AVG(us.Median)) AS Median_Income
+	FROM us_household_income u
+	JOIN us_household_income_statistics us
+		ON u.id = us.id
+	WHERE Mean <> 0 # Removing Outliers
+	GROUP BY u.State_Name, u.City
+	ORDER BY 3 DESC
+	;
+    
+	-- Part 4: Identifying the Average and Median Income Household Income by Area Type
+	SELECT 
+		u.Type,
+        COUNT(u.Type) AS Count_Type,
+		ROUND(AVG(us.Mean)) AS Average_Income,
+		ROUND(AVG(us.Median)) AS Median_Income
+	FROM us_household_income u
+	JOIN us_household_income_statistics us
+		ON u.id = us.id
+	WHERE Mean <> 0
+	GROUP BY u.Type
+    HAVING Count_Type > 100 # Removing Outliers
+	ORDER BY 3 DESC
+	;
